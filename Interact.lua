7118514c4d1268690dc77ea4d5e52d2bec1e1036
Lukas <babecka@outlook.com>
2025-06-19 14:18:35 +0200

    add warning when mod is not loaded

--- Interact.lua
+++ Interact.lua
@@ -1,1 +1,16 @@
 BINDING_HEADER_INTERACT = 'Interact'
+
+StaticPopupDialogs['INTERACT_WARNING'] = {
+   text = 'Failed to load |cffffd200Interact|cffffffff. Follow the installation instructions to ensure proper installation.',
+   button1 = 'Okay',
+   OnAccept = function()
+      DisableAddOn('Interact')
+      ReloadUI()
+   end,
+   timeout = 0,
+   whileDead = true,
+}
+
+if not InteractNearest then
+   StaticPopup_Show('INTERACT_WARNING')
+end
